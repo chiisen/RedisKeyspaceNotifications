bef4597e772523bb198d634f6a5767aa876b3d4c
chiisen <chiisen@hotmail.com>
2024-08-30 00:53:26 +0800

    ✨ feat: 新增範例程式碼與說明

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # RedisKeyspaceNotifications
-Redis Keyspace Notifications 的 C# 範例程式
+使用 StackExchange.Redis 庫在 C# 中監聽 Redis Keyspace Notifications 的程式碼範例，並且設定監聽的 key 為 "eventnews"  
+
+# 介紹
+1. Keyspace Notifications:  
+- Keyspace Notifications 允許您監聽 Redis 數據庫中 key 的變化情況，如 key 的設置（set）、刪除（del）、過期（expire）等事件。
+- 通過 Keyspace Notifications，您可以實現對特定 key 的變化進行監控，並在特定事件發生時接收通知。
+- Keyspace Notifications 更適合用於監控單個 key 的變化，以實現細粒度的監控和操作。
+
+2. PUBLISH(發佈) 和 SUBSCRIBE(訂閱):
+
+- PUBLISH 和 SUBSCRIBE 是 Redis 中的發布-訂閱模式，用於實現消息的發布和訂閱功能。
+- 通過 PUBLISH 命令將消息發布到指定的頻道，而通過 SUBSCRIBE 命令訂閱一個或多個頻道，從而接收相應的消息通知。
+- PUBLISH 和 SUBSCRIBE 更適合用於實現消息的廣播和訂閱，例如實時數據推送、即時通訊等場景。
+
+# Redis 設置
+C:\Program Files\Redis\redis.windows-service.conf 搜尋 notify-keyspace-events 並修改為 KEA  
+```config
+notify-keyspace-events "KEA"
+```
+修改完配置後，保存文件並重新啟動 Redis 服務，讓新的配置生效。
+Win+R 輸入 services.msc 開啟服務管理員，找到 Redis 服務，右鍵選取重新啟動。  
+![重新啟動 Redis 服務](./images/Redis_Resetting.png)
+
---
+++ RedisKeyspaceNotifications/Program.cs
@@ -0,0 +1,35 @@
+﻿using StackExchange.Redis;
+
+class Program
+{
+    static void Main()
+    {
+        // 建立 Redis 連接
+        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+
+        // 創建一個訂閱者
+        var subscriber = redis.GetSubscriber();
+
+        IDatabase db = redis.GetDatabase(0);
+        string valueFirst = db.StringGet("eventnews");
+        Console.WriteLine($"'eventnews': {valueFirst}");
+
+        // 設置 Keyspace Notifications，監聽 key 為 "eventnews" 的事件
+        subscriber.Subscribe("__keyspace@0__:eventnews", (channel, message) =>
+        {
+            if (message == "set")
+            {
+                // key 的值被設置，獲取並列印新的值
+                db = redis.GetDatabase(0);
+                string value = db.StringGet("eventnews");
+                Console.WriteLine($"key 為 eventnews 的值已被設置為: {value}");
+            }
+        });
+
+        Console.WriteLine("正在監聽 key 為 eventnews 的值變化，按 Enter 鍵退出。");
+        Console.ReadLine();
+
+        // 關閉 Redis 連接
+        redis.Close();
+    }
+}
